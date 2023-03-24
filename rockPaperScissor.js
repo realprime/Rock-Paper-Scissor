1749b06198d7b5c57bf7cfcf844278317c66e18e
realprime <hemanthsaikiran12@gmail.com>
2023-03-25 03:42:00 +0530

    Add 'computerChoice' variable and 'playRound()' function

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -9,3 +9,30 @@ function getComputerChoice() {
     let choice = arr[randomIndex].toLowerCase();
     return choice;
 }
+
+const computerChoice = getComputerChoice();
+
+function playRound(userChoice, computerChoice) {
+    console.log(userChoice + " " + computerChoice);
+    if (userChoice === 'rock' && computerChoice === 'paper') {
+        return "You Lose! Paper beats Rock.";
+    }
+    else if (userChoice === 'rock' && computerChoice === 'scissor') {
+        return "You Win! Rock beats Scissor.";
+    }
+    else if (userChoice === 'paper' && computerChoice === 'rock') {
+        return "You Win! Paper beats Rock.";
+    }
+    else if (userChoice === 'paper' && computerChoice === 'scissor') {
+        return "You Lose! Scissor beats Paper.";
+    }
+    else if (userChoice === 'scissor' && computerChoice === 'rock') {
+        return "You Lose! Rock beats Scissor.";
+    }
+    else if (userChoice === 'scissor' && computerChoice === 'paper') {
+        return "You Win! scissor beats paper.";
+    }
+    else if (userChoice === computerChoice) {
+        return "It is a tie.";
+    }
+}
