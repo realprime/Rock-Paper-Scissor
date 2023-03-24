a6b8325769280c79628b5a2d1cde8c2ea6c01518
realprime <hemanthsaikiran12@gmail.com>
2023-03-25 03:54:36 +0530

    Add 'score' dictionary variable and score increments to 'playRound()' function

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -1,3 +1,7 @@
+var score = {
+    playerScore: 0,
+    computerScore: 0
+};
 
 let userInput = prompt(`Please enter your choice. Your choices are:
 1. Rock  2.Paper  3.Scissor`);
@@ -15,24 +19,32 @@ const computerChoice = getComputerChoice();
 function playRound(userChoice, computerChoice) {
     console.log(userChoice + " " + computerChoice);
     if (userChoice === 'rock' && computerChoice === 'paper') {
+        score.computerScore += 1;
         return "You Lose! Paper beats Rock.";
     }
     else if (userChoice === 'rock' && computerChoice === 'scissor') {
+        score.playerScore += 1;
         return "You Win! Rock beats Scissor.";
     }
     else if (userChoice === 'paper' && computerChoice === 'rock') {
+        score.playerScore += 1;
         return "You Win! Paper beats Rock.";
     }
     else if (userChoice === 'paper' && computerChoice === 'scissor') {
+        score.computerScore += 1;
         return "You Lose! Scissor beats Paper.";
     }
     else if (userChoice === 'scissor' && computerChoice === 'rock') {
+        score.computerScore += 1;
         return "You Lose! Rock beats Scissor.";
     }
     else if (userChoice === 'scissor' && computerChoice === 'paper') {
+        score.playerScore += 1;
         return "You Win! scissor beats paper.";
     }
     else if (userChoice === computerChoice) {
+        score.computerScore += 1;
+        score.playerScore += 1;
         return "It is a tie.";
     }
 }
