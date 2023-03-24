b138e5fd964155792d90ff6324ddb9b405c7ffbe
realprime <hemanthsaikiran12@gmail.com>
2023-03-25 03:37:51 +0530

    Add getComputerChoice() function

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -2,3 +2,10 @@
 let userInput = prompt(`Please enter your choice. Your choices are:
 1. Rock  2.Paper  3.Scissor`);
 let userChoice = userInput.toLowerCase();
+
+function getComputerChoice() {
+    arr = ['rock', 'paper', 'scissor']
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    let choice = arr[randomIndex].toLowerCase();
+    return choice;
+}
